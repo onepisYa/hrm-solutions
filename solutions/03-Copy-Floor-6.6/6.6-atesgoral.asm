c506acfb1e986651f92b1b5e2e78c6851f67f8c0
onepisYa <pis1@qq.com>
2025-09-20 18:09:53 +0800

    Add Chinese comments to 6.6-atesgoral.asm
    Added Chinese comments explaining the constraints and instructions for the Copy-Floor exercise, clarifying the requirements for code length, operation count, and ignoring INBOX.

--- solutions/03-Copy-Floor-6.6/6.6-atesgoral.asm
+++ solutions/03-Copy-Floor-6.6/6.6-atesgoral.asm
@@ -2,7 +2,9 @@
 -- 03-Copy-Floor - SIZE 6/6 - SPEED 6/6 --
 
 -- The copyfrom command is introduced. Copy the letters B U G to the outbox.
-
+-- 最多使用 6 行代码
+-- 最多进行 6 步运算
+-- 暂时无视 INBOX , 把下面的三个字母直接送到 OUTBOX 里面。 BUG(这三个字母)
     COPYFROM 4
     OUTBOX
     COPYFROM 0
