b90aec0cd1aa9deda8af9271a82f42d14dfc38e2
onepisYa <pis1@qq.com>
2025-09-20 17:47:00 +0800

    Add Chinese instructional comments to ASM file
    Added Chinese comments to provide guidance on assembling the program and clarify the task for processing INBOX and OUTBOX operations.

--- solutions/01-Mail-Room-6.6/6.6-onepisya.asm
+++ solutions/01-Mail-Room-6.6/6.6-onepisya.asm
@@ -2,7 +2,8 @@
 -- 01-Mail-Room - SIZE 6/6 - SPEED 6/6 --
 
 -- Straightforward. The first level introduces the inbox and outbox commands.
-
+-- 把代码拖到区域中来组成程序。
+-- 你的程序必须能告诉你、对于 INBOX(输入栏) 里面的每个东西、你都需要取出来、再丢进 OUTBOX(输出栏) 里。
     INBOX
     OUTBOX
     INBOX
